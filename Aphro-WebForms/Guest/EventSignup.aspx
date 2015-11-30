66a1f0d5d5afa438be8d24bf50e1bbf400f265bb
Scott Brady <scott@prolio.com>
2015-11-30 09:42:24 -0600

    Added Individual Event Page

---
+++ Aphro-WebForms/Guest/EventSignup.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Event Signup" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EventSignup.aspx.cs" Inherits="Aphro_WebForms.Guest.EventSignup" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="EventName" runat="server"></asp:Label>
+    <br />
+    <asp:Label ID="EventDescription" runat="server"></asp:Label>
+    <br />
+    <asp:Label ID="EventLocation" runat="server"></asp:Label>
+    <br />
+    <asp:Label ID="EventPrice" runat="server"></asp:Label>
+    <br />
+    <asp:Label ID="EventPrimePrice" runat="server"></asp:Label>
+</asp:Content>
---
+++ Aphro-WebForms/Guest/EventSignup.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using AutoMapper;
+using Oracle.DataAccess.Client;
+
+namespace Aphro_WebForms.Guest
+{
+    public partial class EventSignup : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Request.QueryString["Event"]))
+            {
+                var eventId = long.Parse(Request.QueryString["Event"]);
+
+                DataTable eventTable = new DataTable();
+                List<Models.Event> eventModel = new List<Models.Event>();
+
+                using (OracleConnection objConn = new OracleConnection(Global.ConnectionString))
+                {
+                    // Set up the upcomingEvents command
+                    var eventCommand = new OracleCommand("TICKETS_QUERIES.getEvent", objConn);
+                    eventCommand.BindByName = true;
+                    eventCommand.CommandType = CommandType.StoredProcedure;
+                    eventCommand.Parameters.Add("p_Return", OracleDbType.RefCursor, ParameterDirection.ReturnValue);
+                    eventCommand.Parameters.Add("p_EventId", OracleDbType.Int64, eventId, ParameterDirection.Input);
+
+                    try
+                    {
+                        // Execute the queries and auto map the results to models
+                        objConn.Open();
+                        var eventAdapter = new OracleDataAdapter(eventCommand);
+                        eventAdapter.Fill(eventTable);
+                        eventModel = Mapper.DynamicMap<IDataReader, List<Models.Event>>(eventTable.CreateDataReader());
+                    }
+                    catch (Exception ex)
+                    {
+                        throw (ex);
+                    }
+
+                    objConn.Close();
+                }
+
+                // Fill list dropdowns with data from the database
+                if (eventModel.Count > 0)
+                {
+                    var currentEvent = eventModel.FirstOrDefault();
+                    EventName.Text = currentEvent.name;
+                    EventDescription.Text = currentEvent.description;
+                    EventLocation.Text = currentEvent.building;
+                    EventPrice.Text = "$" + currentEvent.regular_price;
+                    EventPrimePrice.Text = "$" + currentEvent.prime_price;
+                }
+            }
+        }
+    }
+}

8d6e56c411ca33915a7560e61afb0432e294cbb3
crdnblu <crdnblu@ymail.com>
2016-02-14 22:45:36 -0600

    changed header portal

--- Aphro-WebForms/GuestPortal.Master.designer.cs
+++ Aphro-WebForms/GuestPortal.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Aphro_WebForms {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderSection;
         
         /// <summary>
+        /// guestName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label guestName;
+        
+        /// <summary>
         /// masterForm control.
         /// </summary>
         /// <remarks>

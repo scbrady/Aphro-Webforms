d23e7ba94d954b5e4fbbf1cf8807f0cbc4b65d04
Scott Brady <scott@prolio.com>
2016-04-02 10:26:42 -0500

    Simplified Button Code

--- Aphro-WebForms/Scripts/plus_minus_buttons.js
+++ Aphro-WebForms/Scripts/plus_minus_buttons.js
@@ -1,199 +1,96 @@
-﻿$('.sub-qty').css("opacity", ".5");
-$('.subfaculty').css("opacity", ".5");
-
-$('.add-qty').click(function (e) {
-    // Stops the button from being a button
-    e.preventDefault();
-    $('.sub-qty').css("opacity", "1");
-    // Set the needed variables
-    field_name = $(this).attr('field');
-    var max_val = 10;
-    var group_size = 0;
-    if (!isNaN(group_size = parseInt($("#MainContent_GroupSize").text(), 10)) ||
-        !isNaN(group_size = parseInt($("#MainContent_GroupSize").val(),  10)))
-        max_val = 10 - group_size;
-    var current_val = parseInt($('#' + field_name).val());
-    var another_val = parseInt($('#MainContent_FacultyTicketsSize').val());
-
-    // Fixes for Employee Portal
-    if ($("#MainContent_FacultyTicketsSize").length)
-    {
-        if (field_name === "MainContent_GuestTicketsSize")
-        {
-            var faculty_ticket_size;
-            if (!isNaN(faculty_ticket_size = parseInt($("#MainContent_FacultyTicketsSize").val(), 10)))
-                max_val = 10 - group_size - faculty_ticket_size;
-        }
-        else if (field_name === "MainContent_FacultyTicketsSize") {
-            var guest_ticket_size;
-            if (!isNaN(guest_ticket_size = parseInt($("#MainContent_GuestTicketsSize").val(), 10)))
-                max_val = 10 - group_size - guest_ticket_size;
-        }
-    }
-
-    // Make sure counter can't go higher than max_val
-    if (!isNaN(current_val) && current_val < max_val) {
-        $(".add-qty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".sub-qty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        if (another_val == 0)
-        {
-            $(".subfaculty").addClass("disabled");
-            $("input.disabled").css("opacity", "0.5");
-        }
-
-        if (current_val == (max_val - 1))
-        {
-            $(".add-qty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
-            $(".addfaculty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
-        }
-        $('#' + field_name).val(current_val + 1);
-    } else if (current_val == max_val) {
-        $(".add-qty").addClass("disabled");
-        $("input.disabled").css("opacity", ".5");
-        $('#' + field_name).val(max_val);
-    } else {
-        // Just in case something goes wrong, you get 0. Sorry 
-        $('#' + field_name).val(0);
-    }
-});
-
-$(".sub-qty").click(function (e) {
-    // Stops the button from being a button
-    e.preventDefault();
-    $('.add-qty').css("opacity", "1");
-
-    // Set the needed variables
-    field_name = $(this).attr('field');
+﻿$(function () {
     var min_val = 0;
     if (!$("#MainContent_GroupSize").length)
         min_val = 1;
-    var current_val = parseInt($('#' + field_name).val());
-    var another_val = parseInt($('#MainContent_FacultyTicketsSize').val());
 
-    // Make sure counter can't go lower than min_val
-    if (!isNaN(current_val) && current_val > min_val) {
-        $(".sub-qty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".add-qty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".addfaculty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
+    var max_tickets = 10;
+    var at_max = false;
+    disableEnableButtons();
 
-        if (another_val == 0) {
-            $(".subfaculty").addClass("disabled");
-            $("input.disabled").css("opacity", "0.5");
+    $('.add-qty').click(function (e) {
+        // Stops the button from being a button
+        e.preventDefault();
+
+        // Set the needed variables
+        var field_name = $(this).attr('field');
+        var current_val = tryParseInt($('#' + field_name).val());
+        didHitMax();
+
+        // Make sure counter can't go higher than the max tickets allowed
+        if (!at_max) {
+            $('#' + field_name).val(current_val + 1);
+        } else {
+            $('#' + field_name).val(current_val);
         }
 
+        // Disable or Enable buttons based on the new value
+        disableEnableButtons();
+    });
 
-        if (current_val == (min_val + 1))
-        {
-            $(".sub-qty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
+    $(".sub-qty").click(function (e) {
+        // Stops the button from being a button
+        e.preventDefault();
+
+        // Set the needed variables
+        var field_name = $(this).attr('field');
+        var current_val = tryParseInt($('#' + field_name).val());
+
+        // Make sure counter can't go lower than min_val
+        if (current_val > min_val) {
+            $('#' + field_name).val(current_val - 1);
+        } else {
+            $('#' + field_name).val(min_val);
         }
-        $('#' + field_name).val(current_val - 1);
-    } else {
-        $(".sub-qty").addClass("disabled");
-        $("input.disabled").css("opacity", ".5");
-        $('#' + field_name).val(min_val);
+
+        // Disable or Enable buttons based on the new value
+        disableEnableButtons();
+    });
+
+    function didHitMax() {
+        var group_size;
+        var extra_tickets = 0;
+
+        if ((group_size = tryParseInt($("#MainContent_GroupSize").text())) == 0)
+            group_size = tryParseInt($("#MainContent_GroupSize").val(), 10);
+
+        $('.add-qty').each(function () {
+            var field_name = $(this).attr('field');
+            var current_val = tryParseInt($('#' + field_name).val());
+            extra_tickets += current_val;
+        });
+
+        at_max = group_size + extra_tickets >= max_tickets;
+    }
+
+    function disableEnableButtons() {
+        // Disable all add buttons if at the max number of tickets allowed
+        // Else enable all of them
+        didHitMax();
+        if (at_max)
+            $('.add-qty').prop("disabled", true);
+        else
+            $('.add-qty').prop("disabled", false);
+
+        // Loop through all the subtract buttons
+        $('.sub-qty').each(function () {
+            var field_name = $(this).attr('field');
+            var current_val = parseInt($('#' + field_name).val());
+
+            // If input is equal to the min, disable button
+            // Else enable it
+            if (current_val <= min_val)
+                $(this).prop("disabled", true);
+            else
+                $(this).prop("disabled", false);
+        });
     }
 });
 
-$('.addfaculty').click(function (e) {
-    // Stops the button from being a button
-    e.preventDefault();
-    $('.subfaculty').css("opacity", "1");
-    // Set the needed variables
-    field_name = $(this).attr('field');
-    var max_val = 10;
-    var group_size = 0;
-    if (!isNaN(group_size = parseInt($("#MainContent_GroupSize").text(), 10)) ||
-        !isNaN(group_size = parseInt($("#MainContent_GroupSize").val(), 10)))
-        max_val = 10 - group_size;
-    var current_val = parseInt($('#' + field_name).val());
-    var another_val = parseInt($('#MainContent_GuestTicketsSize').val());
-
-    // Fixes for Employee Portal
-    if ($("#MainContent_FacultyTicketsSize").length) {
-        if (field_name === "MainContent_GuestTicketsSize") {
-            var faculty_ticket_size;
-            if (!isNaN(faculty_ticket_size = parseInt($("#MainContent_FacultyTicketsSize").val(), 10)))
-                max_val = 10 - group_size - faculty_ticket_size;
-        }
-        else if (field_name === "MainContent_FacultyTicketsSize") {
-            var guest_ticket_size;
-            if (!isNaN(guest_ticket_size = parseInt($("#MainContent_GuestTicketsSize").val(), 10)))
-                max_val = 10 - group_size - guest_ticket_size;
-        }
-    }
-
-    // Make sure counter can't go higher than max_val
-    if (!isNaN(current_val) && current_val < max_val) {
-        $(".addfaculty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".subfaculty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-
-        if (another_val == 0)
-        {
-            $(".sub-qty").addClass("disabled");
-            $("input.disabled").css("opacity", "0.5");
-        }
-
-        if (current_val == (max_val - 1)) {
-            $(".addfaculty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
-            $(".add-qty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
-        }
-        $('#' + field_name).val(current_val + 1);
-    } else if (current_val == max_val) {
-        $(".addfaculty").addClass("disabled");
-        $("input.disabled").css("opacity", ".5");
-        $('#' + field_name).val(max_val);
-    } else {
-        // Just in case something goes wrong, you get 0. Sorry bud
-        $('#' + field_name).val(0);
-    }
-});
-$(".subfaculty").click(function (e) {
-    // Stops the button from being a button
-    e.preventDefault();
-    $('.addfaculty').css("opacity", "1");
-
-    // Set the needed variables
-    field_name = $(this).attr('field');
-    var min_val = 0;
-    if (!$("#MainContent_GroupSize").length)
-        min_val = 1;
-    var current_val = parseInt($('#' + field_name).val());
-    var another_val = parseInt($('#MainContent_GuestTicketsSize').val());
-
-    // Make sure counter can't go lower than min_val
-    if (!isNaN(current_val) && current_val > min_val) {
-        $(".subfaculty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".addfaculty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-        $(".add-qty").removeClass("disabled");
-        $("input.disabled").css("opacity", "1");
-
-        if (another_val == 0) {
-            $(".sub-qty").addClass("disabled");
-            $("input.disabled").css("opacity", "0.5");
-        }
-
-        if (current_val == (min_val + 1)) {
-            $(".subfaculty").addClass("disabled");
-            $("input.disabled").css("opacity", ".5");
-        }
-        $('#' + field_name).val(current_val - 1);
-    } else {
-        $(".subfaculty").addClass("disabled");
-        $("input.disabled").css("opacity", ".5");
-        $('#' + field_name).val(min_val);
-    }
-});
+function tryParseInt(number) {
+    var parsed;
+    parsed = parseFloat(number);
+    if (!isNaN(parsed))
+        return parsed;
+    else
+        return 0;
+}
